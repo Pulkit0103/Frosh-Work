3a5f74acc559fe5246c70f772ad6e5fc83fcf681
Pulkit0103 <74561711+Pulkit0103@users.noreply.github.com>
2021-08-10 22:10:28 +0530

    Recruiting Form

---
+++ Frosh Form/script.js
@@ -0,0 +1,10 @@
+var Form = document.getElementById("Submitbtn");
+
+Submitbtn.onclick = function(){
+    alert("Your Form is Successfully Submitted!");
+}
+
+// function myFunction(){
+//     alert("Your Form is Successfully Submitted!");
+
+// }
